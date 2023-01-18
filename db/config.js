baf1f25f1553211338301be8b6c8edf3bd429599
david cuspoca <david-cuspoca@hotmail.com>
2023-01-17 18:54:14 -0600

    cambio stricquqery false

--- db/config.js
+++ db/config.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 
 const connection = async() => {
   try {
+    mongoose.set('strictQuery', false);
     await mongoose.connect(process.env.DATABASE_URL, {
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     })
+
+
   } catch (error) {
     console.log(error)
     throw new Error('No se pudo iniciar la db')
